cacb2ce1eeb6a2a99d1992ae3425fc915e105f23
Deon <deonsummers91@gmail.com>
2016-05-25 22:23:38 -0500

    initial commit

---
+++ Hot100.js
@@ -0,0 +1,25 @@
+'use strict'
+var request = require('request');
+var cheerio = require('cheerio');
+var $;
+
+request('http://www.billboard.com/charts/hot-100', function(err, resp, body) {
+	$ = cheerio.load(body);
+	ParseData();
+})
+
+
+var Artists = {};
+var Hot100 = [];
+function ParseData() {
+	$('.chart-row__artist').each(function() {
+		let that = $(this);
+		let artist = that.html().trim();
+		let song =  that.prev().html().trim();
+		Hot100.push(artist+' - '+song);
+		Artists[artist] = Artists.hasOwnProperty(artist) ? Artists[artist]+1 : 1;
+	});
+	console.log('Top Song is:'+Hot100[0]);
+	console.log('Rihanna has '+Artists['Rihanna']+' songs on the Hot 100');
+}
+
