ebc98b6aae96bed9e2a1df52e37ad57f34971d21
dsummers91 <deonsummers91@gmail.com>
2016-06-06 18:32:29 -0500

    Archived Calls working

--- data/Hot100.js
+++ data/Hot100.js
@@ -5,20 +5,33 @@ var cheerio = require('cheerio');
 var Artist = require('../models/artist');
 var Promise = require('promise');
 
-function list() {
+function list(date) {
 	return new Promise(function(resolve, reject) {
-		list = [];
-		request('http://www.billboard.com/charts/hot-100', function(err, resp, body) {
+		var lists = [];
+		date = date ? new Date(date) : new Date();
+		var y = date.getFullYear();
+		var m = date.getUTCMonth()+1;
+		var d = date.billboardUpdateDay();
+		if(m < 10) {m = '0'+m};
+		if(d < 10) {d = '0'+d};
+		request('http://www.billboard.com/charts/hot-100/'+y+'-'+m+'-'+d, function(err, resp, body) {
 			var $ = cheerio.load(body);
 			$('.chart-row__artist').each(function() {
 				let self = $(this);
 				let artist = self.html().trim();
 				let song =  self.prev().html().trim();
-				list.push([artist, song]);
+				lists.push([artist, song]);
 			});
-			return resolve(list);
+			return resolve({lists: lists, date: new Date(y+'/'+m+'/'+d)});
 		});
 });
+}
+
+Date.prototype.billboardUpdateDay = function() {
+	while(this.getUTCDay()!==6) {
+		this.setUTCDate(this.getUTCDate() + 1);
+	}
+	return this.getUTCDate();
 }
 
 module.exports = list
